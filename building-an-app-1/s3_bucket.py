26dc36eac788638403880644546bc99588393631
Numaan Bashir Mir <71112748+mirnumaan@users.noreply.github.com>
2023-03-22 12:34:47 +1100

    final changes

--- building-an-app-1/s3_bucket.py
+++ building-an-app-1/s3_bucket.py
@@ -1,11 +1,10 @@
-from flask import Flask,render_template, request, redirect, session
+# from flask import Flask,render_template, request, redirect, session
 import boto3
 import logging
 from botocore.exceptions import ClientError 
 
-
-
-def create_bucket(bucket_name, region = 'us-east-1'):
+region_name = 'us-east-1'
+def create_bucket(bucket_name, region=None):
     """Create an S3 bucket in a specified region
 
     If a region is not specified, the bucket is created in the S3 default
@@ -22,10 +21,15 @@ def create_bucket(bucket_name, region = 'us-east-1'):
             s3_client = boto3.client('s3')
             s3_client.create_bucket(Bucket=bucket_name)
         else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
-                                    CreateBucketConfiguration=location)
+            access_key="ASIAR44X363U7GP2W6MF"
+            secret_key="H//ASuVFAF7dCWneTWYTHqgLbQ+YkHWb8CgA/75c"   
+            aws_session_token="FwoGZXIvYXdzEOD//////////wEaDACcm2AmwJfjDGNsCCLNAUftifEzGGZZCBOOXm153gojkQ8bV/wOpS32PwHv2H6/9mixmYSSjFZyqft1AhDav5nLsvgzT6MdmDPI9sWB4Z5VS3q9ztabbXgeH6XnGN/GbyWB7yaVwQjyBRGgyn0uNnP1me7Y7Rti89EX5ZvDLLROkIUuh1qocJVqn+r3mCGU7bgSX68xsbW6O6fB+ZyFgIF0RMnQbcn5a/JI3yMAcv4w9Z9KUNypSXS6NRwbEJpdk0yaKVPhuRyOlmw2RJKvVaOc5rA5xc185YGFzZsoguDooAYyLc+KRKs8NrVg6X5jo4i+VzvT0xNWH87+pQUxvqVluzxbSNTL9VJPSLN9fzcbLw=="
+            s3_client = boto3.client('s3', 
+                                     aws_access_key_id=access_key,
+                                     aws_secret_access_key=secret_key,
+                                     aws_session_token = aws_session_token,region_name=region)
+            
+            s3_client.create_bucket(Bucket=bucket_name)
     except ClientError as e:
         logging.error(e)
         return False
@@ -33,17 +37,4 @@ def create_bucket(bucket_name, region = 'us-east-1'):
 
 if __name__ == '__main__':
     # AWS Credentials
-    access_key="ASIAR44X363U5N2K53VF"
-    secret_key="QTFe3YejAiB+3IKtASnsbCWW2LRJ6szMV1HPOOhC"
-    aws_session_token="FwoGZXIvYXdzEM///////////wEaDM+dqQAQc/h3MhlPHyLNAVz0nCbwldv+O7MuhzKMubVA470+ttrGLmfKhBPay2drmZH5bEijT0yGL/zslPKtLYV0ICgsQwVa5FtMxlKjlVZRyUS0V3y5ELJz3LsAvq8IpC+APSUFzXbYs7GPlaevBwhVb7Th1EN/DRjV5o7cvTED7/q64x6mkpWM2jSkBt1rZ++Qv5vB/R8ESl0/A5kPiFnHshyKx3P5npv9Tt/JnrSYj3V2Wt+4kUr9Sj11pImmQFnpNyl6zLrdVLWwhWcSrndL3YXKy8sWy2XNvp4okYzloAYyLRSQ6eawzuS6zqveuxBBC4ZIH8zvtojV7RSddcYTaj97jOyKFI6/RfN49JSlew=="
-    region_name = 'us-east-1'
-    table_name = 'login'
-
-    # DynamoDB Client
-    dynamodb = boto3.resource('dynamodb',
-                            aws_access_key_id=access_key,
-                            aws_secret_access_key=secret_key,
-                            aws_session_token = aws_session_token,
-                            region_name=region_name)
-    
-    create_bucket('hello_world')
+    create_bucket('halayolay006', region=region_name)
